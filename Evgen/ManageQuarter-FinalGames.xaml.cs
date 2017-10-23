0b2ba179f07beda6cd282a2c869035a3711d9cf4
ноут <@MPC>
2017-10-23 17:06:26 +0500

    Теперь норм

--- Evgen/ManageQuarter-FinalGames.xaml.cs
+++ Evgen/ManageQuarter-FinalGames.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace UEFA
+namespace Football
 {
     /// <summary>
     /// Логика взаимодействия для Window1.xaml
