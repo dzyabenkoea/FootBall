585ed4dbf85535f73b0e7b93f71363712d41b609
Алексей <namaleksey1996@live.ru>
2017-10-23 16:31:20 +0500

    прост

--- Управления командами/ManageTeams.xaml.cs
+++ Управления командами/ManageTeams.xaml.cs
@@ -34,22 +34,35 @@ namespace Football
 
         void LoadTeams()
         {
-            DataTable dt = DBAdapter.DB.RunSelect("Select ID_Team As ID, flag_url, TeamName As Team, countrycode As Code From [Teams]");
-            DataGrid1.ItemsSource = dt.DefaultView;
-            
-
+            DataGrid1.ItemsSource = DBAdapter.DB.RunSelect("Select ID_Team As ID, flag_url, TeamName As Team, countrycode As Code, regoin From [Teams]").DefaultView;
+            DataGrid1.Columns[4].Visibility = Visibility.Hidden;
         }
 
         private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            DataGrid1.ItemsSource = DBAdapter.DB.RunSelect("Select ID_Team As ID, flag_url, TeamName As Team, countrycode As Code From [Teams] Where TeamName = '"+ SearchTextBox.Text +"'").DefaultView;
+            DataGrid1.ItemsSource = DBAdapter.DB.RunSelect("Select ID_Team As ID, flag_url, TeamName As Team, countrycode As Code, regoin From [Teams] Where TeamName = '" + SearchTextBox.Text +"'").DefaultView;
         }
 
         private void AddBut_Click(object sender, RoutedEventArgs e)
         {
-            ManageTeams form = new Football.ManageTeams();
+            AddEditTeam form = new AddEditTeam();
             form.Show();
             DBAdapter.DB.RunInsert("Insert Into [Teams] (flag_url, TeamName, countrycode, region) values('" + SearchTextBox.Text+ "','" + SearchTextBox.Text + "','" + SearchTextBox.Text + "','" + SearchTextBox.Text + "'))");
+        }
+
+        private void EditBut_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataGrid1.SelectedIndex != -1)
+            {
+                int i = DataGrid1.SelectedIndex;
+                AddEditTeam form = new AddEditTeam();
+                form.Show();
+            }
+            else
+            {
+                MessageBox.Show("Выберите команду для редактирования");
+            }
+
         }
     }
 }
