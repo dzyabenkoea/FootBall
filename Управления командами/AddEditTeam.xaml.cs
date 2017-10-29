e6607aa23ce0184fda4df02fdea002346caacf3f
Алексей <namaleksey1996@live.ru>
2017-10-29 16:03:15 +0500

    Добавлено редактирование/добавление команды

--- Управления командами/AddEditTeam.xaml.cs
+++ Управления командами/AddEditTeam.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using System.Windows.Controls;
 
 namespace Football
 {
@@ -25,13 +27,17 @@ namespace Football
         }
 
         bool AddEdit = false;
-        string url, region;
+        string url, region, idTeam;
 
-        private void EditBut_Copy_Click(object sender, RoutedEventArgs e)
+        private void SaveAndCloseBut_Click(object sender, RoutedEventArgs e)
         {
             if(!AddEdit)
             {
-                DBAdapter.DB.RunInsert("Insert Into [Teams] (flag_url, TeamName, countrycode, region) values('" + CountryCodeTextBox.Text + "','" + CountryCodeTextBox.Text + "','" + CountryCodeTextBox.Text + "','" + CountryCodeTextBox.Text + "'))");
+                DBAdapter.DB.RunInsert("Insert Into [Teams] (flag_url, TeamName, countrycode, region) values('" + ImageCountry.Source + "','" + TeamNameTextBox.Text + "','" + CountryCodeTextBox.Text + "','" + RegionComBox.SelectedValue + "'))");
+            }
+            else
+            {
+                DBAdapter.DB.RunInsert("Update[Teams] Set flag_url = '" + TeamNameTextBox + "', TeamName = '" + TeamNameTextBox.Text + "', countrycode = '" + TeamNameTextBox.Text + "', region = '" + RegionComBox.SelectedValue + "' WHERE ID_Team = '" + idTeam + "'");
             }
 
             Close();
@@ -44,19 +50,75 @@ namespace Football
 
         private void DeleteBut_Click(object sender, RoutedEventArgs e)
         {
+            int i = DataGrid1.SelectedIndex;
+            if (i != -1)
+            {
+                if (MessageBox.Show("Удалить?", "", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                    DBAdapter.DB.RunInsert("Delete From [Players] Where ID_Player = '" + ((DataRowView)DataGrid1.Items[i]).Row[0].ToString() + "'");
+            }
+            else
+            {
+                MessageBox.Show("Выберите команду для удаления");
+            }
+        }
 
+        private void AddBut_Click_1(object sender, RoutedEventArgs e)
+        {
+            AddEditPlayer form = new AddEditPlayer();
+            form.Show();
+        }
+
+        private void EditBut_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataGrid1.SelectedIndex != -1)
+            {
+                int i = DataGrid1.SelectedIndex;
+                AddEditPlayer form = new AddEditPlayer(((DataRowView)DataGrid1.Items[i]).Row[0].ToString(), ((DataRowView)DataGrid1.Items[i]).Row[2].ToString(), ((DataRowView)DataGrid1.Items[i]).Row[1].ToString(), ((DataRowView)DataGrid1.Items[i]).Row[3].ToString(), ((DataRowView)DataGrid1.Items[i]).Row[4].ToString(), Convert.ToDateTime(((DataRowView)DataGrid1.Items[i]).Row[5]));
+                form.Show();
+            }
+            else
+            {
+                MessageBox.Show("Выберите игрока для редактирования");
+            }
+        }
+
+        private void CloseBut_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void SelectFlagButton_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog myDialog = new Microsoft.Win32.OpenFileDialog();
+            myDialog.Filter = "Картинки(*.JPG;*.PNG)|*.JPG;*.PNG";
+            myDialog.CheckFileExists = true;
+            myDialog.Multiselect = true;
+            if (myDialog.ShowDialog() == true)
+            {
+                TeamNameTextBox.Text = myDialog.FileName;
+            }
         }
 
         public AddEditTeam(string idTeam, string Url, string TeamName, string Region, string CountryCode)
         {
             InitializeComponent();
             AddEdit = true;
+            //ImageCountry.Source = BitmapFrame.Create(new Uri(@"pack://siteoforigin:,,,/Resources/" + Url));
             url = Url;
+            this.idTeam = idTeam;
             region = Region;
             TeamNameTextBox.Text = TeamName;
             CountryCodeTextBox.Text = CountryCode;
+            url = url.Substring(url.IndexOf('\\') + 1);
         }
 
-       
+        void LoadPlayersTeam()
+        {
+            DataGrid1.ItemsSource = DBAdapter.DB.RunSelect("Select ID_Player As ID, firstname as [First name],  lastname As [Last name] , shirt_number As [Shirt number], position As Position, date_of_birth as [Date of birth] From [Players] where team_id = '" + idTeam + "'").DefaultView;
+            DataGrid1.Columns[0].Visibility = Visibility.Hidden;
+           //var s = ImageCountry.Source;
+        }
+
+
     }
 }
