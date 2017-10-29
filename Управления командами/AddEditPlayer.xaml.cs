23658f8561463626e8db44c3be233f2144cf83b7
Алексей <namaleksey1996@live.ru>
2017-10-29 14:28:12 +0500

    Редактирование формы команд

--- Управления командами/AddEditPlayer.xaml.cs
+++ Управления командами/AddEditPlayer.xaml.cs
@@ -23,5 +23,10 @@ namespace Football
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
