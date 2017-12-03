73db80ddc55837b37e1d2e09f9fd71a64e5178bb
Егор <gorich60@gmail.com>
2017-12-03 20:19:05 +0500

    Работает добавление и обновление Чемпионатов

---
+++ Управление чемпионатами/TeamParticipant.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Football
+{
+    public class TeamParticipant
+    {
+        int id;
+        string name;
+        bool participates;
+
+        public TeamParticipant(int id, string name, bool participates)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Participates = participates;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public bool Participates
+        {
+            get { return participates; }
+            set { participates = value; }
+        }
+    }
+}
